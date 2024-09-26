0c2669db79536b092b839294b6e2d185538ceb01
Fedor Nikiforov <Tyuiu.NikiforovFA@outlook.com>
2024-09-26 18:11:47 +0500

    Task4

--- Tyuiu.NikiforovFA.Sprint2.Task4.V3.Lib/DataService.cs
+++ Tyuiu.NikiforovFA.Sprint2.Task4.V3.Lib/DataService.cs
@@ -7,7 +7,7 @@ namespace Tyuiu.NikiforovFA.Sprint2.Task4.V3.Lib
         public double Calculate(double x, double y)
         {
             double z;
-            return z = x < (y - 1) ? (y + (y - 1) / (x + 2)) : (x + 2 * y * (1 / (y + 3)));
+            return Math.Round(z = x < (y - 1) ? (y + (y - 1) / (x + 2)) : (x + 2 * y * (1 / (y + 3))), 3);
         }
     }
 }
