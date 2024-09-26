bdb3f570f64953ddaddf05ae042e6677d3b51604
Fedor Nikiforov <Tyuiu.NikiforovFA@outlook.com>
2024-09-26 14:28:49 +0500

    Task1

--- Tyuiu.NikiforovFA.Sprint2.Task1.V17/Program.cs
+++ Tyuiu.NikiforovFA.Sprint2.Task1.V17/Program.cs
@@ -12,7 +12,7 @@ namespace Tyuiu.NikiforovFA.Sprint2.Task1.V17
             Console.WriteLine("* Выполнил: Никифоров Федор Александрович | ПКТб-24-1                     *");
             Console.WriteLine("***************************************************************************");
             int a = 125, b = 123, c = 455, d = 321;
-            Console.WriteLine("a = {0}, b = {1}, c = {2}, d = {3}", a,b,c,d);
+            Console.WriteLine("a = {0}, b = {1}, c = {2}, d = {3}", a, b, c, d);
             Console.WriteLine("* Результат:                                                              *");
             for (int i = 0; i < 6; i++)
             {
