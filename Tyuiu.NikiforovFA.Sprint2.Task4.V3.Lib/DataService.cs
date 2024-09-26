0ab22b8ba05b035639eb7898ef90409b83f70903
Fedor Nikiforov <Tyuiu.NikiforovFA@outlook.com>
2024-09-26 18:09:30 +0500

    Task4

---
+++ Tyuiu.NikiforovFA.Sprint2.Task4.V3.Lib/DataService.cs
@@ -0,0 +1,13 @@
+﻿using tyuiu.cources.programming.interfaces;
+using tyuiu.cources.programming.interfaces.Sprint2;
+namespace Tyuiu.NikiforovFA.Sprint2.Task4.V3.Lib
+{
+    public class DataService : ISprint2Task4V3
+    {
+        public double Calculate(double x, double y)
+        {
+            double z;
+            return z = x < (y - 1) ? (y + (y - 1) / (x + 2)) : (x + 2 * y * (1 / (y + 3)));
+        }
+    }
+}
