810ef1a3b223599d55d413d2cc44c672de1494da
Fedor Nikiforov <Tyuiu.NikiforovFA@outlook.com>
2024-10-02 20:57:12 +0500

    Task5

--- Tyuiu.NikiforovFA.Sprint2.Task5.V7.Lib/DataService.cs
+++ Tyuiu.NikiforovFA.Sprint2.Task5.V7.Lib/DataService.cs
@@ -6,47 +6,28 @@ namespace Tyuiu.NikiforovFA.Sprint2.Task5.V7.Lib
     {
         public string FindMonthName(int startYear, int n)
         {
-            double y = n % 12;
             string m = "";
             if (n > 12)
             {
-                switch (y)
-                {
-                    case 1: m = "Январь"; break;
-                    case 2: m = "Февраль"; break;
-                    case 3: m = "Март"; break;
-                    case 4: m = "Апрель"; break;
-                    case 5: m = "Май"; break;
-                    case 6: m = "Июнь"; break;
-                    case 7: m = "Июль"; break;
-                    case 8: m = "Август"; break;
-                    case 9: m = "Сентябрь"; break;
-                    case 10: m = "Октябрь"; break;
-                    case 11: m = "Ноябрь"; break;
-                    case 12: m = "Декабрь"; break;
-                }
-                return m;
+                n %= 12;
             }
-            else
+            switch (n)
             {
-                switch (n)
-                {
-                    case 1: m = "Январь"; break;
-                    case 2: m = "Февраль"; break;
-                    case 3: m = "Март"; break;
-                    case 4: m = "Апрель"; break;
-                    case 5: m = "Май"; break;
-                    case 6: m = "Июнь"; break;
-                    case 7: m = "Июль"; break;
-                    case 8: m = "Август"; break;
-                    case 9: m = "Сентябрь"; break;
-                    case 10: m = "Октябрь"; break;
-                    case 11: m = "Ноябрь"; break;
-                    case 12: m = "Декабрь"; break;
-                }
-                return m;
+                case 0: m = "Январь"; break;
+                case 1: m = "Февраль"; break;
+                case 2: m = "Март"; break;
+                case 3: m = "Апрель"; break;
+                case 4: m = "Май"; break;
+                case 5: m = "Июнь"; break;
+                case 6: m = "Июль"; break;
+                case 7: m = "Август"; break;
+                case 8: m = "Сентябрь"; break;
+                case 9: m = "Октябрь"; break;
+                case 10: m = "Ноябрь"; break;
+                case 11: m = "Декабрь"; break;
             }
-            
+            return m;
+
         }
     }
 }
